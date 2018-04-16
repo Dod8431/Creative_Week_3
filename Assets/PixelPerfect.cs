38a19edae10a4e5e6d83a14695c3b8473ab9cc4f
Dod8431 <32450556+Dod8431@users.noreply.github.com>
2018-04-16 15:07:36 +0200

    CC

--- Assets/PixelPerfect.cs
+++ Assets/PixelPerfect.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class PixelPerfect : MonoBehaviour {
 
-	void Start () 
-	{
-		
-	}
-
-	void Update () 
-	{
-		
-	}
-
 	void LateUpdate()
 	{
-
+		transform.SetPositionAndRotation (new Vector3 (Mathf.FloorToInt (transform.localPosition.x), transform.localPosition.y, (Mathf.FloorToInt (transform.localPosition.z * 1.155f) / 1.155f)), transform.localRotation);
 	}
 }
