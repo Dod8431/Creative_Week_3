f441bf11478b1e758ceef1d2cac226a1855cabdb
Axel-Arnaud <arnaudaxrl.pro@gmail.com>
2018-04-16 12:50:11 +0200

    yo

---
+++ Assets/Script/Game_Manager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour {
+    public bool day = true;
+    public bool night = false;
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Cycle");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (day)
+        {
+
+        }else
+        {
+
+        }
+		
+	}
+
+    IEnumerator Cycle()
+    {
+        yield return new WaitForSeconds(60);
+        day =! day;
+        night =! night;
+    }
+
+    void Dayfunction()
+    {
+
+
+        StartCoroutine ("Cycle");
+    }
+
+    void Nightfunction()
+    {
+
+
+        StartCoroutine("Cycle");
+    }
+
+}
