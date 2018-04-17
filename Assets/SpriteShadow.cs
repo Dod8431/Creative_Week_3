70cb67c0df5cb1abc0c49d5255e34b6da9f96243
Dod8431 <32450556+Dod8431@users.noreply.github.com>
2018-04-17 11:06:27 +0200

    DD

---
+++ Assets/SpriteShadow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteShadow : MonoBehaviour {
+
+	public Vector2 offset = new Vector2 (-3,-3);
+
+	private SpriteRenderer sprRndCaster;
+	private SpriteRenderer sprRndShadow;
+
+	private Transform transCaster;
+	private Transform transShadow;
+}
