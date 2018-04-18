d80bfb5240635479aeffd303e1a8e9aa203980c5
Axel-Arnaud <arnaudaxrl.pro@gmail.com>
2018-04-18 10:11:04 +0200

    ouai debug encore

--- Assets/Script/Zombie.cs
+++ Assets/Script/Zombie.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Zombie : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    SpriteRenderer zombiesp;
+    public GameObject player;
+    public int vie = 3;
+    public GameObject GC;
+    // Use this for initialization
+    void Start () {
+        zombiesp = this.GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Player");
+        GC = GameObject.Find("GameController");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (this.transform.position.x < player.transform.position.x)
+        {
+            zombiesp.flipX = true;
+        }
+        else
+        {
+            zombiesp.flipX = false;
+        }
+
+        if (vie <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+        
+        if (GetComponent<Game_Manager>().day == true)
+        {
+            StartCoroutine(Burn());
+        }
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            vie -= 1;
+        }
+    }
+
+    IEnumerator Burn()
+    {
+        yield return new WaitForSeconds(1.5f);
+        vie -= 1;
+    }
 }
